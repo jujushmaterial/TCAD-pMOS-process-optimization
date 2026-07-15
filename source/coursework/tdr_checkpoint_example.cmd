26af71e8fdb732108cbbf407e232120db29c9c88
Sanghyeon Ju <hiimang3@gmail.com>
2026-07-16 08:37:03 +0900

    Add preliminary TDR command example

---
+++ source/coursework/tdr_checkpoint_example.cmd
@@ -0,0 +1,4 @@
+# Preliminary coursework example from the final report.
+# The @node@ placeholder places the Workbench node number in the file name.
+
+struct tdr=My_Project_n@node@_10_Al_depo !Gas !interfaces
